ea55e2e6ff787fb6b28b68be6e328e880f82ce4e
Arinze Stanley Ebeledike <ebeledikea@gmail.com>
2022-11-19 18:54:10 +0100

    I included a footer component containing working social icons

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,45 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+
 const Footer = () => {
-    return ( 
-        <footer className="footer-container">
-            Copyright 2022
-        </footer>
-     );
-}
- 
+  return (
+    <footer className="footer-container">
+      <div class="social-container">
+        <a href="https://facebook.com/taliusdike" className="facebook-social">
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+      </div>
+
+      <div class="social-container">
+        <a
+          href="https://www.twitter.com/taliusdike_"
+          className="twitter-social"
+        >
+          <FontAwesomeIcon icon={faTwitter} size="2x" />
+        </a>
+      </div>
+
+      <div class="social-container">
+        <a
+          href="https://www.instagram.com/taliusdike"
+          className="instagram-social"
+        >
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </a>
+      </div>
+
+      <div class="social-container">
+        <a href="https://www.github.com/taliusdike" className="github-social">
+          <FontAwesomeIcon icon={faGithub} size="2x" />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
 export default Footer;
--- src/Components/Info.js
+++ src/Components/Info.js
@@ -12,11 +12,12 @@ const Info = () => {
       <section className="header-texts">
         <h1>Talius Dike</h1>
         <p className="header-paragraph-1">Frontend Developer</p>
-        <p className="header-paragraph-2">www.taliusdike.com</p>
+        {/* <p className="header-paragraph-2">www.taliusdike.com</p> */}
+        <a href="www.taliusdike.disha.page" className="header-paragraph-2">www.taliusdike.com</a>
       </section>
       <section className="header-buttons">
-        <button className="header-button-1">Email</button>
-        <button className="header-button-2">LinkedIn</button>
+        <a href="mailto:ebeledikea@gmail.com"> <button className="header-button-1">Email</button></a>
+        <a href="www.linkedin.com/in/taliusdike"> <button className="header-button-2">LinkedIn</button></a>
       </section>
     </div>
   );
