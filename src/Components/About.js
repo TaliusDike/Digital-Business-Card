3b146d6d659f9a4babddbabed309418594f95472
Arinze Stanley Ebeledike <ebeledikea@gmail.com>
2022-11-21 21:11:16 +0100

    I included a netlify.toml file for deployment.

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,7 +2,7 @@ const About = () => {
     return ( 
         <section className='main-about-container'>
             <h1>About</h1>
-            <p>I am a frontend developer (in training) with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            <p>I am a Frontend Dev (in training) with an interest in simplifying processes. I'm currently putting in the work to successfully morph from a career in writing for marketing to writing codes, while keeping up with industry best practices.</p>
         </section>
 
      );
--- src/Components/Interests.js
+++ src/Components/Interests.js
@@ -2,7 +2,9 @@ const Interests = () => {
     return ( 
         <section className="main-interests-container">
             <h1>Interests</h1>
-        <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        <p>
+        Film enthusiast: I hope to make my own film someday. Classical music lover. Reader. Lover. Fried-chicken lover. FM radio buff. Travel geek.
+                 </p>
         </section>
      );
 }
